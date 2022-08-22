af3f23f2a8213b235a10246e203009933235b956
jaikrishnaverma <jaikrishnaverma>
2022-08-22 16:51:32 +0530

    componenet 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Component1 from './components/Componenet1';
 import Component2 from './components/Component2';
 import Component3 from './components/Component3';
+import Component4 from './components/Component4';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
        <Component1 />
        <Component2 />
        <Component3 />
+       <Component4 />
       </header>
     </div>
   );
---
+++ src/components/Component4.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+
+class Component4 extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                <div className='box box3 box4 box5' >
+                    <h1>Pick Your Premium</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quas.</p>
+                    <div className='mid third fourthrow'>
+
+                        <div className="card">
+                            <span className="span1" style={{ height: "130px", display: "block" }}>
+                                <button className='firstbtn'>One-time plans availbale</button>
+                                <h4>Mini</h4>
+                                <p>From &#8377;7/day</p>
+                                <p>1 account</p>
+                            </span>
+                            <hr />
+
+                            <span className="span2" style={{ height: "200px", display: "block",marginBottom:"20px" }}>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Group Session</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Download 30 songs on 1 mobile device</p></span>
+                            </span>
+                            <button className='plans'>VIEW PLANS</button>
+                            <p className='tc'><a href="">Terms and conditions apply.</a></p>
+                        </div>
+
+                        <div className="card">
+                            <span className="span1" style={{ height: "130px", display: "block" }}>
+                                <button className='firstbtn xy'>1 Month Free</button>
+                                <button className='firstbtn'>One-time plans availbale</button>
+                                <h4>Individual</h4>
+                                <p>&#8377;199/month after offer</p>
+                                <p>1 account</p>
+                            </span>
+                            <hr />
+                            <span className="span2" style={{ height: "200px", display: "block",marginBottom:"20px" }}>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Group Session</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Download 30 songs on 1 mobile device</p></span>
+                            </span>
+
+                            <button className='plans'>VIEW PLANS</button>
+
+                            <p className='tc'><a href="">Terms and conditions apply.</a> 1month free not available for users who have already tried premium.</p>
+                        </div>
+
+                        <div className="card">
+                            <span className="span1" style={{ height: "130px", display: "block" }}>
+                                <button className='firstbtn xy'>1 Month Free</button>
+                                <button className='firstbtn'>One-time plans availbale</button>
+                                <h4>Duo</h4>
+                                <p>&#8377;149/month after offer</p>
+                                <p>2 accounts</p>
+                            </span>
+                            <hr />
+                            <span className="span2" style={{ height: "200px", display: "block",marginBottom:"20px" }}>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                            </span>
+
+                            <button className='plans'>VIEW PLANS</button>
+                            <p className='tc'><a href="">Terms and conditions apply.</a> 1month free not available for users who have already tried premium.</p>
+                      </div>
+
+                        <div className="card">
+                            <span className="span1" style={{ height: "130px", display: "block" }}>
+                                <button className='firstbtn xy'>1 Month Free</button>
+                                <button className='firstbtn'>One-time plans availbale</button>
+                                <h4>Family</h4>
+                                <p>&#8377;179/month after offer</p>
+                                <p>Up to 6 accounts</p>
+                            </span>
+                            <hr />
+                            <span className="span2" style={{ height: "200px", display: "block",marginBottom:"20px" }}>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                                <span style={{ display: "flex", alignItems: "center", margin: "2px 0px" }}><p style={{ fontSize: "25px", padding: "5px", paddingLeft: "0px" }}>&#x2713;</p><p>Add free music listening on mobile</p></span>
+                            </span>
+
+                            <button className='plans'>VIEW PLANS</button>
+                            <p className='tc'><a href="">Terms and conditions apply.</a> 1month free not available for users who have already tried premium.</p>
+                      </div>
+                    </div>
+                    <div className="flex">
+                        <h4>Special discount for eligible students in university.   </h4>
+                        <button>LEARN MORE</button>
+                    </div>
+
+                </div>
+            </>
+        );
+    }
+}
+
+export default Component4;
